d667ac158f2f3cd1abf2be1bd684ab411182ce6f
bibihehe <anh.bd194216@sis.hust.edu.vn>
2022-08-16 16:34:55 +0700

    first commit

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+
+            string v1 = System.Configuration.ConfigurationManager.AppSettings["k1"];
+            string v2 = System.Configuration.ConfigurationManager.AppSettings["k2"];
+            string v3 = System.Configuration.ConfigurationManager.AppSettings["k3"];
+
+            Console.WriteLine(v1 + " " + v2 + " " + v3);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timerVlog = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteLog("Service started at " + DateTime.Now);
+            timerVlog.Elapsed += new ElapsedEventHandler(TimeInterval);
+            timerVlog.Interval = 10000;
+            timerVlog.Enabled = true;
+        }
+
+        private void TimeInterval(object source,ElapsedEventArgs e)
+        {
+            WriteLog("Service is recall at " + DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            WriteLog("Service stopped at " + DateTime.Now);
+        }
+        private void WriteLog (string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\";
+            if(!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            string file = path + DateTime.Now.ToString("dd-MM-yyy") + ".txt";
+            if (!System.IO.File.Exists(file))
+            {
+                using(StreamWriter sw = File.CreateText(file))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(file))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+
+        }
+
+    }
+}
